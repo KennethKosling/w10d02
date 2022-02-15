cdedf583a8b8b931d50e862b6480ad99f9a903cf
KennethKosling <kenkoz7164@gmail.com>
2022-02-14 21:32:57 -0500

    Magic 8 Ball express application created

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const tipCalc = require('./tip')
+const eightBall = require('./magic')
 
 app.get('/greeting/:name', (req, res) => {
     res.send(`<h1>Yo! Welcome to the spot, ${req.params.name}</h1>`)
@@ -8,6 +9,14 @@ app.get('/greeting/:name', (req, res) => {
 
 app.get('/tip/:total/:tipPercentage', (req, res) => {
     res.send(`<h1>${tipCalc(req.params.total, req.params.tipPercentage)}</h1>`)
+})
+
+app.get('/magic/:question', (req, res) => {
+    res.send(`
+    ${req.params.question}?
+    <h1>${eightBall.random}</h1>
+    Disclaimer: The only way to change the message is to restart the server.
+    `)
 })
 
 app.listen(3000)
